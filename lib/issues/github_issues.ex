dee2b1c76e2a6b99fcc373415c53e94da197758d
mfunaro <mfunaro@gmail.com>
2015-08-01 00:22:18 -0400

    fix typo in github_issues.ex

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -8,7 +8,7 @@ defmodule Issues.GithubIssues do
     end
 
     def issues_url(user, project) do
-      "https://api.github.com/repos/#{user}/#{project}/issue"
+      "https://api.github.com/repos/#{user}/#{project}/issues"
     end
     def handle_response(%{status_code: 200, body: body}), do: {:ok, body}
     def handle_response(%{status_code: ___, body: body}), do: {:error, body}
